2bd95f1d20cca2f966477a8764c089782ff48e63
Hyoyeon Lee <lee190928@gmail.com>
2023-03-18 16:56:06 +0000

    first commit

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -0,0 +1,37 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.annotation.Nonnull;
+
+
+import io.atlassian.fugue.Pair;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import uk.ac.bris.cs.scotlandyard.model.*;
+
+public class MyAi implements Ai {
+
+	@Nonnull @Override public String name() { return "SIGMA"; }
+
+	@Nonnull @Override public Move pickMove(
+			@Nonnull Board board,
+			Pair<Long, TimeUnit> timeoutPair) {
+		// returns a random move, replace with your own implementation
+		var moves = board.getAvailableMoves().asList();
+		return moves.get(new Random().nextInt(moves.size()));
+	}
+
+	private Integer getScore(Board.GameState state, Move move){
+		return null;
+	}
+
+	private Integer ScoreLocation(/*mrX's location, detectives' location*/){
+		return null;
+	}
+
+
+
+}
