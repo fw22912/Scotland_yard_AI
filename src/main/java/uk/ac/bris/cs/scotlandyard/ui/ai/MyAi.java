b9f29637f574e0fe9b319084587c64e3784d1b77
averyryu <avery.r.engineer@gmail.com>
2023-04-09 23:37:05 +0100

    Modified minimax and getOptimalMoves

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -47,31 +47,31 @@ public class MyAi implements Ai {
 	}
 
 	//MiniMax
-	private Integer miniMax(Board board, int depth, int alpha, int beta, Boolean checkMrX) {
-		Board.GameState gameState = (Board.GameState) board;
+	public Integer minimax(Board board, Move move, int depth, int alpha, int beta, boolean checkMrX) {
 		if (depth == 0 || !board.getWinner().isEmpty()) {
-			return (int)Double.NEGATIVE_INFINITY;
+			return updateLocation(move);
 		}
 		if (checkMrX) {
-			int maxEval = (int) Double.NEGATIVE_INFINITY;
-			for (Move move : board.getAvailableMoves()) {
-				Board newBoard = updatedBoard(board, move);
-				int eval = miniMax(newBoard, depth - 1, alpha, beta, false);
-				//can I compare double and integer!!!!
+			int maxEval = Integer.MIN_VALUE;
+			for (Move child : board.getAvailableMoves()) {
+				int eval = minimax(board, child, depth - 1, alpha, beta, false);
 				maxEval = Math.max(maxEval, eval);
 				alpha = Math.max(alpha, maxEval);
-				if (beta <= alpha) break;
+				if (beta <= alpha) {
+					break;
+				}
 			}
 			return maxEval;
 		}
 		else {
-			int minEval = (int) Double.POSITIVE_INFINITY;
-			for (Move move : board.getAvailableMoves()) {
-				Board newBoard = updatedBoard(board, move);
-				int eval = miniMax(newBoard, depth - 1, alpha, beta, true);
+			int minEval = Integer.MAX_VALUE;
+			for (Move child : board.getAvailableMoves()) {
+				int eval = minimax(board, child, depth - 1, alpha, beta, true);
 				minEval = Math.min(minEval, eval);
 				beta = Math.min(beta, minEval);
-				if (beta <= alpha) break;
+				if (beta <= alpha) {
+					break;
+				}
 			}
 			return minEval;
 		}
@@ -79,25 +79,25 @@ public class MyAi implements Ai {
 
 
 	//chooses the best move for MrX
-	private List<Move> scoreToMoves(Board board, int depth) {
+	private List<Move> getOptimalMoves(Board board, int depth) {
 		int minEval = (int) Double.POSITIVE_INFINITY;
 		int alpha = (int) Double.NEGATIVE_INFINITY;
 		int beta = (int) Double.POSITIVE_INFINITY;
-		List<Move> firstMoves = new ArrayList<>();
+		List<Move> optimalMoves = new ArrayList<>();
 
 		for (Move move : board.getAvailableMoves()) {
 			Board updated = updatedBoard(board, move);
 			//do minimax with updatedBoard after designated move
-			int eval = miniMax(updated, depth - 1, alpha, beta, true);
+			int eval = minimax(updated, move, depth, alpha, beta, true);
 			if (minEval > eval) {
 				minEval = eval;
-				firstMoves.clear();
-				firstMoves.add(move);
+				optimalMoves.clear();
+				optimalMoves.add(move);
 			} else if (minEval == eval) {
-				firstMoves.add(move);
+				optimalMoves.add(move);
 			}
 		}
-		return firstMoves;
+		return optimalMoves;
 	}
 
 
@@ -105,9 +105,9 @@ public class MyAi implements Ai {
 	private Move mrXBestMove(ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, Board board, int depth) {
 		Move finalMove;
 		int score = 0;
-		List<Move> firstMoves = scoreToMoves(board, depth);
-		System.out.println("firstMoves: " + firstMoves);
-		List<Move> noAdjacent = checkAdjacent(board, firstMoves);
+		List<Move> optimalMoves = getOptimalMoves(board, depth);
+		System.out.println("firstMoves: " + optimalMoves);
+		List<Move> noAdjacent = checkAdjacent(board, optimalMoves);
 		System.out.println("noAdjacent: " + noAdjacent);
 		List<Move> highestScore = new ArrayList<>();
 		List<Move> finalMoves = new ArrayList<>();
@@ -129,8 +129,8 @@ public class MyAi implements Ai {
 		int score2 = 0;
 		//If there are more than possible moves, randomly choose among those moves
 		if(noAdjacent.isEmpty()){
-			int randomIndex = ran.nextInt(firstMoves.size());
-			finalMove = firstMoves.get(randomIndex);
+			int randomIndex = ran.nextInt(optimalMoves.size());
+			finalMove = optimalMoves.get(randomIndex);
 		}
 		else if(highestScore.size() > 1){
 			// TODO : 파이널무브즈 수 하나 이상일 때 겟어베일러블무브 수 비교해서 가장 높은 걸로
@@ -241,7 +241,6 @@ public class MyAi implements Ai {
 				case TAXI -> ticketVal += 2;
 				case BUS -> ticketVal += 4;
 				case UNDERGROUND -> ticketVal += 8;
-				case SECRET -> ticketVal += 5;
 			}
 		}
 		return ticketVal;
@@ -252,9 +251,8 @@ public class MyAi implements Ai {
 	//It returns the distance from the detectives' location to mrX's destination
 	private Integer calculateDistance(Board board, Move move, ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph) {
 		List<Integer> detectivesLocation = getDetectivesLocation(board);
-		int totalVal = 0;
 		int size = 0;
-		List<List<Integer>> listOfPath = new ArrayList<>();
+		List<List<Integer>> shortPath = new ArrayList<>();
 		List<List<Integer>> allPath = new ArrayList<>();
 
 		for (Integer detectiveLocation : detectivesLocation) {
@@ -288,13 +286,13 @@ public class MyAi implements Ai {
 				path.add(node);
 				node = preNode.get(node);
 			}
-			if(path.size() < 4){
-				listOfPath.add(path);
+			if(path.size() < 5){
+				shortPath.add(path);
 			}
 			allPath.add(path);
 			System.out.println("path: " + path);
 		}
-		size = listOfPath.isEmpty() ? allPath.stream().mapToInt(List::size).sum() : listOfPath.stream().mapToInt(List::size).sum();
+		size = shortPath.isEmpty() ? allPath.stream().mapToInt(List::size).sum() : shortPath.stream().mapToInt(List::size).sum();
 		return size;
 	}
 }
